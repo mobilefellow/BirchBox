60be5bb17d9a763017637caaf76221935eefa6e9
Steven Wang <gaocuom@outlook.com>
2016-02-20 13:28:26 -0500

    Initial Commit

---
+++ BirchBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BirchBox
+//
+//  Created by Steven on 2/20/16.
+//  Copyright © 2016 Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BirchBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BirchBox
+//
+//  Created by Steven on 2/20/16.
+//  Copyright © 2016 Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
